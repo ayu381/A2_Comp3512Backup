d2449d0ac2f758482f701a84a7da81c131771e82
ayu381 <91147708+ayu381@users.noreply.github.com>
2023-11-20 16:46:09 -0700

    Implemented artist and genre list

--- js-files/SearchBrowse.js
+++ js-files/SearchBrowse.js
@@ -1,7 +1,7 @@
 const songs = JSON.parse(songContent);
 
 // Function to populate rows with song data
-function updateSongTable() {
+function songDisplay() {
     const tableBody = document.querySelector("#search-results tbody");
 
     // Iterate through each song and create a new row in the table
@@ -21,8 +21,51 @@ function updateSongTable() {
         yearCell.textContent = song.year;
         row.appendChild(yearCell);
 
+        const genreCell = document.createElement("td");
+        genreCell.textContent = song.genre.name;
+        row.appendChild(genreCell);
+
+        const popularityCell = document.createElement("td");
+        popularityCell.textContent = song.details.popularity; 
+        row.appendChild(popularityCell);
+
         tableBody.appendChild(row);
     });
 }
 
-window.onload = updateSongTable;
+const artists = JSON.parse(artistContent);
+
+// Artist select options function
+function artistOptions() {
+    const artistSelect = document.getElementById("artist-select");
+
+    // Create an option for each artist and add it to the select element
+    artists.forEach((artist) => {
+        const option = document.createElement("option");
+        option.value = artist.name;
+        option.textContent = artist.name;
+        artistSelect.appendChild(option);
+    });
+}
+
+const genres = JSON.parse(genreContent);
+
+// Genre select options function
+function genreOptions() {
+    const genreSelect = document.getElementById("genre-select");
+
+    // Create an option for each genre and add it to the select element
+    genres.forEach((genre) => {
+        const option = document.createElement("option");
+        option.value = genre.name;
+        option.textContent = genre.name;
+        genreSelect.appendChild(option);
+    });
+}
+
+// Call all functions the window is loaded
+window.onload = function () {
+    songDisplay();
+    artistOptions();
+    genreOptions();
+};
